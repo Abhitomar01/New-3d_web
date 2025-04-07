b9cced7cd87265c867d809a61124048d7dd70de5
Abhitomar01 <tabhi1957@gmail.com>
2025-04-07 22:09:14 +0530

    UI completed

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ headings.forEach((heading) => {
     });
 });
 
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+       
+      });
+}
+swiperAnimation();
